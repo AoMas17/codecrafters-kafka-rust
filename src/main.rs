1e6a8d4d9438f0655bb984f4c6a6973f8fe7e7ea
alexandre.santacreu <alexandre.santacreu@epita.fr>
2025-01-28 00:01:41 +0100

    Send Correlation ID

--- src/main.rs
+++ src/main.rs
@@ -1,13 +1,15 @@
 #![allow(unused_imports)]
-use std::net::TcpListener;
+use std::{io::Write, net::TcpListener};
 
 fn main() {
     let listener = TcpListener::bind("127.0.0.1:9092").unwrap();
 
     for stream in listener.incoming() {
         match stream {
-            Ok(_stream) => {
+            Ok(mut _stream) => {
                 println!("accepted new connection");
+                let buffer: [u8; 8] = [0, 0, 0, 0, 0, 0, 0, 7];
+                _stream.write(&buffer).unwrap();
             }
             Err(e) => {
                 println!("error: {}", e);
